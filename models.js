d38a13dedc2ea9110a743350a93344e8690d15cc
P-WM <P-WM@users.noreply.github.com>
2017-09-11 16:02:24 +0100

    Store dates as ms since epoch

--- models.js
+++ models.js
@@ -8,7 +8,7 @@ const uniq = require('lodash/uniq');
 exports.Tweet = class Tweet {
   constructor(tweet) {
     this.id_str = tweet.id_str;
-    this.created_at = new Date(tweet.created_at);
+    this.created_at = new Date(tweet.created_at).valueOf();
     this.full_text = tweet.truncated ? tweet.extended_tweet.full_text : tweet.text;
     this.short_text = tweet.truncated ? tweet.text : null;
     this.sender = new exports.User(tweet.user);
@@ -21,7 +21,7 @@ exports.User = class User {
   constructor(user) {
     this.name = user.name;
     this.id_str = user.id_str;
-    this.created_at = new Date(user.created_at);
+    this.created_at = new Date(user.created_at).valueOf();
     this.screen_name = user.screen_name;
     this.location = user.location;
     this.followers_count = user.followers_count;
